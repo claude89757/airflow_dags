f4d53a416bc21dce2b38d4ecb2f42fdc5c0c8441
claude89757 <138977524+claude89757@users.noreply.github.com>
2024-12-30 19:44:52 +0800

    fix

--- dags/sh_tennis/sh_002_watcher.py
+++ dags/sh_tennis/sh_002_watcher.py
@@ -931,7 +931,7 @@ def check_tennis_courts():
 
 # 创建DAG
 dag = DAG(
-    dag_id='sh_tennis_watcher',
+    dag_id='上海青少体育网球场巡检',
     default_args=default_args,
     description='监控网球场地可用情况',
     schedule_interval='*/10 * * * *',  # 每3分钟执行一次
